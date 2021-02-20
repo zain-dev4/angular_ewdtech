8845f7f6168fc0055adc81b2fda87ad7211e8601
bbpsystems <78722488+bbpsystems@users.noreply.github.com>
2021-02-20 16:52:18 +0500

    sadfasdfasdfasfasdfas

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -31,8 +31,35 @@ app.controller("contactmainController", function ($scope) {
   };
 });
 app.controller("aboutusmainController", function ($scope) {
-  $scope.name = function () {
-    var zain = "aboutusmainController";
-    return zain;
+  $scope.aboutusObj = {
+    ourstory: `OUR STORY?`,
+    AfterStory: `EWD Tech was established in 2015 in Faisalabad, Industrial hub of Pakistan. Our goal was to provide services regarding designing and development in the emerging digital industry for our clients locally and internationally. Today many professionals and business owners have endorsed EWD Tech for its services. EWD Tech will continue to improve its services and provide support to its clients.`,
+    since: `Since`,
+    sincedate: `2015`,
+    Employ: `Employ`,
+    TotalEmploy: `40+`,
+    deleverproject: `Total Project we Deliverd`,
+    Totaldeleverproject: `40`,
+    howwedo: `HOW WE DO ?`,
+    howwedo1st: `EWD tech employs hard working team of graphic designers, web designers and IT experts who provide quality service and solutions. Our solutions are very cost effective and efficient that are suitable for both small and big corporations and also businesses.`,
+    askcustomerwhattheywant: `Ask customer what they want`,
+    askcustomerwhattheywantDetail: `We ask customer all queries regarding the problem or solution they want so we don’t miss out any details. We make full effort to implement what customer has asked and not leave any detail behind. We keep in contact with customer and inform them about the progress`,
+    qualityDesighn: `Quality design and user-friendly solution `,
+    qualityDesighndetail: ` . Our top priority is to provide best quality solution to our customer, we use latest technology and software available to implement user friendly customized solutions that pleases our client.`,
+    testandimplemention: `Testing and Implementation. `,
+    testandimplementiondetail: `Once we have designed the solution, we test it at our end several times to see if we can further improve it or if there is any problem in it because sending customer the final design. If you want any changes, we will still do that for you.`,
+    costomersopport: `Customer Support .`,
+    costomersopprtDetail: `We hold customer satisfaction in high regard. Every service we provide whether it’s designing, web development or expert guidance until the customer is completely satisfied and informs us, we are available 24/7 to help in any way possible. `,
+    ourstrenght: "OUR STRENGTH",
+    ourstrenghtDetail:
+      "We are a pioneer in providing outclass and world standard web based solutions for small to large corporate companies. We have genius and talented designers, programmers, and marketing executives so to lead our clients in their web ventures. We take each new opportunity and implement it to the improvement of overall development procedures",
+    multifashionandsolution: `MULTIFACETED PLANS AND SOLUTIONS`,
+    multifashionandsolutionDetail: `EWD Web Solutions delivers services and enables clients to cut down costs and provides an up-to-date application. We consider hearing out our clients to understand their requirements and prerequisites. We make smart research and deep analysis then plan the feasible best solutions that are based on innovative technologies.`,
+    ENHANCEDACCURACY: `ENHANCED ACCURACY`,
+    ENHANCEDACCURACYDetail: `Ensure quality and successful project outcomes by engaging our software testing experts. Our QA team has much knowledge and is aware of latest technologies and standards to analyze the process rapidly so to get 100% guarantee on quality. We consider meeting quality principles keeping in mind the end goal to deliver an adaptable and vigorous application.`,
+    TRANSPARENTCOLLABORATION: `TRANSPARENT COLLABORATION`,
+    TRANSPARENTCOLLABORATIONDetail: `We happily assist our clients and share free ideas for the victory of their business. Depending on specific requirements, our experts will take clients through each phase of solution development, step-by-step, providing sound advice on technology and application design options. We will respond quickly to your emails and high priority calls.`,
+    OURAPPROACH: `OUR APPROACH`,
+    OURAPPROACHDetail: `EWD Web Solutions aims to be a most important player in overall software development and IT market. Our approach is to build an outstanding website which helps our clients to represent their businesses on the web in an efficient and successful way. We plan out our workflow that's well-organized, planning that's comprehensive and collaboration that's stable.`,
   };
 });
